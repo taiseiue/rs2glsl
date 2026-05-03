7164cfdc3b82de353e38428a8b614cabce6e60ee
taiseiue <taiseiue@wsnet.jp>
2026-05-04 03:15:53 +0900

    うごいた

--- crates/shader-cli/src/main.rs
+++ crates/shader-cli/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, fs};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path = env::args().nth(1).expect("usage: shader-cli <file.rs>");
+    let source = fs::read_to_string(path)?;
+    let glsl = shader_transpiler::transpile_to_glsl(&source)?;
+    println!("{glsl}");
+    Ok(())
 }
--- crates/shader-transpiler/src/lib.rs
+++ crates/shader-transpiler/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod codegen_glsl;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn transpile_to_glsl(source: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let file = syn::parse_file(source)?;
+    codegen_glsl::generate(&file).map_err(Into::into)
 }
